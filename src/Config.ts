ee48c88b1d25fa34391d606b2e3bdc5d57a74bf4
anna-co-kr <81524488+anna-co-kr@users.noreply.github.com>
2021-12-01 23:33:44 +0900

    anna-co-kr
    ver.1

---
+++ src/Config.ts
@@ -0,0 +1,10 @@
+export const WEDDING_INVITATION_URL = 'http://localhost:8080';
+export const KAKAOTALK_API_TOKEN = '카카오톡 API 토큰을 입력해주세요!';
+export const KAKAOTALK_SHARE_IMAGE = '카카오톡으로 공유할 때 사용되는 대표이미지 URL을 입력해주세요!';
+
+export const WEDDING_DATE = '2021년 12월 18일 토요일 오후 5시';
+export const WEDDING_LOCATION = '메리스 에이프럴';
+
+export const GROOM_NAME = '김 이백';
+
+export const BRIDE_NAME = '고 희정';
